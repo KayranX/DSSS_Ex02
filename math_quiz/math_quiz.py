08da1a667ea9d1c350e81829340f2ecdf6d99b7a
Johannes Enk <enk.johannes98@gmail.com>
2023-11-13 20:31:47 +0100

    Cleaned up the provided code.

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,97 @@
 import random
+from typing import Tuple
 
 
-def function_A(min, max):
+def create_random_integer(min_value: int, max_value: int) -> int:
     """
-    Random integer.
+    Create a random integer in range [min_value, max_value].
+
+    Args:
+    :param min_value: Lower bound
+    :param max_value: Upper bound
+    :return: Random integer within bounds
     """
-    return random.randint(min, max)
+    return random.randint(min_value, max_value)
 
 
-def function_B():
+def select_random_operator() -> str:
+    """
+    Select a random arithmetic operator from the set [+, -, *].
+
+    Args:
+    :return: Random arithmetic operator
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def perform_calculation(number_1: int or float, number_2: int or float, operator: str) -> Tuple[str, int]:
+    """
+    Perform the calculations with the given operator on both given numbers.
 
-def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    Args:
+    :param number_1: First number to be used in the calculation
+    :param number_2: Second number to be used in the calculation
+    :param operator: Operator to determine the performed arithmetics
+    :return: Formula for the calculation and its result
+    """
+
+    # create the formula for the computation and store it in a string
+    formula: str = f"{number_1} {operator} {number_2}"
+
+    # perform the calculation and return the formula and the result
+    if operator == '+':
+        result: int = number_1 + number_2
+    elif operator == '-':
+        result: int = number_1 - number_2
+    else:
+        result: int = number_1 * number_2
+    return formula, result
+
+
+def start_math_quiz():
+    """
+    Start a math quiz that will ask the user for solutions to given formulas and calculates a score.
+
+    :return: Function does not return any values
+    """
+
+    # score stores the achieved result and iterations determines the amount of questions asked
+    score: int = 0
+    iterations: int = 10
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    # loop over the number of iterations to ask questions
+    for _ in range(iterations):
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        # determine the values and operator for the questions randomly
+        number_1: int = create_random_integer(1, 10)
+        number_2: int = create_random_integer(1, 6)
+        operator: str = select_random_operator()
 
-        if useranswer == ANSWER:
+        # create the formula that will be given to the user
+        formula, result = perform_calculation(number_1, number_2, operator)
+        print(f"\nQuestion: {formula}")
+
+        # read the input from the user and determine its validity, new prompt if the input is invalid
+        while True:
+            try:
+                user_answer = input("Your answer: ")
+                user_answer = int(user_answer)
+                break
+            except ValueError:
+                print("Please enter an integer!")
+
+        # give the user feedback whether their answer is correct
+        if user_answer == result:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {result}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{iterations}")
+
 
 if __name__ == "__main__":
-    math_quiz()
+    start_math_quiz()
